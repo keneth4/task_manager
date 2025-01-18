1b0b2634847ea7e3916b91904898d573f1c497b7
Keneth <k.ent@hotmail.com>
2025-01-18 20:14:40 +0100

    Set up basic API structure and container

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    print("Application running at: http://localhost:8000")
+
+
+def hello():
+    print("Hello world")
+
+
+@app.get("/")
+async def root():
+    return "Hello world"
